0fa9be7f4509f7e7fcd0ad83c6fbc689b4463b59
Afreed15 <Afreed.vs786@gmail.com>
2021-03-17 15:51:25 +0530

    commit one

---
+++ Data/MoviesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiMovies.Models;
+
+namespace WebApiMovies.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext (DbContextOptions<MoviesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiMovies.Models.Movies> Movies { get; set; }
+    }
+}
